10dd91380a476618cfda87fc2562461bd282e1de
Lula Leus <lula.leus@gmail.com>
2016-06-02 20:11:36 +0300

    Feat: work with inbox messages

--- public/javascripts/controllers/inbox.js
+++ public/javascripts/controllers/inbox.js
@@ -15,5 +15,46 @@ angular.module('paloAltoFrontApp').controller("inboxCtrl", [
         DataHandler.getMessages("received").then(function(response) {
             $scope.receivedMessages  = response;
         });
+
+        function toggle(messages, index) {
+            for (var i = 0; i < messages.length; i++) {
+                // Could be simplified, but left for readability
+                // Readability is important
+                if (index != i || messages[i].isOpen) {
+                    messages[i].isOpen = false;
+                }
+                else {
+                    messages[i].isOpen = true;
+                }
+            }
+        }
+
+        $scope.toggleStar = function($event, type, index) {
+            $event.stopPropagation();
+
+
+            if (type == "sent") {
+                $scope.sentMessages[i].isStarred = !$scope.sentMessages[i].isStarred;
+            }
+            else if (type == "received") {
+                $scope.receivedMessages[i].isStarred = !$scope.receivedMessages[i].isStarred
+            }
+            else {
+                console.warn("Unknown type " + type);
+            }
+        };
+        
+        $scope.toggleMessage = function(type, index) {
+            switch (type) {
+                case "sent":
+                    toggle($scope.sentMessages, index);
+                    break;
+                case "received":
+                    toggle($scope.receivedMessages, index);
+                    break;
+                default:
+                    console.warn("Unknown type " + type);
+            }
+        };
     }
 ]);
--- public/javascripts/directives/menuItem.js
+++ public/javascripts/directives/menuItem.js
@@ -1,9 +1,26 @@
 /**
  * Created by Lula on 6/2/2016.
  */
-angular.module('paloAltoFrontApp').directive('menuItem', [
-    function () {
+angular.module('paloAltoFrontApp').directive('menuItem', ['$location',
+    function ($location) {
         function linker(scope, element, attrs) {
+
+            // Each element decides if it's active or not based on path
+            function toggleActive() {
+                // On first render
+                if (element.find("a").attr("href").indexOf($location.path()) >= 0) {
+                    element.addClass("active");
+                }
+                else {
+                    element.removeClass("active");
+                }
+            }
+
+            scope.$on("$routeChangeSuccess", function() {
+               toggleActive();
+            });
+
+            // On menu click
             element.on("click", function(){
                 element.addClass("active");
                 element.siblings().removeClass("active");
